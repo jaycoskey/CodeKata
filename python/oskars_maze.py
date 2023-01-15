21972b7b474320ee35b2b442bf377b46e54770d1
Jay Coskey <jay.coskey@gmail.com>
2023-01-15 03:52:10 -0800

    Remove non-branching nodes; switch to undirected graph; write dotfile

--- python/oskars_maze.py
+++ python/oskars_maze.py
@@ -10,6 +10,7 @@
 # Coordinates used range from 0 to 10 for each of the x, y, and z axes.
 
 import networkx as nx
+from networkx.drawing.nx_agraph import write_dot
 
 import numpy as np
 from numpy.ma import make_mask
@@ -65,50 +66,54 @@ def assert_nodes_are_in_maze(maze, nodes):
     assert(all(are_corners_in_maze))
 
 
-def cube_to_maze(xyz):
-    def tup_sum(a, b):
-        return (a[0]+b[0], a[1]+b[1], a[2]+b[2])
-
-    node_pos = list(zip(*np.where(~xyz)))  # Record their positions
-    nbr_vecs = [(-1,0,0), (1,0,0)] + [(0,-1,0), (0,1,0)] + [(0,0,-1), (0,0,1)]
-
-    maze = nx.DiGraph()
-    for from_pos in node_pos:
-        for nbr_vec in nbr_vecs:
-            to_pos = tup_sum(from_pos, nbr_vec)
-            if to_pos in node_pos:  # Found an open connector (edge) of the maze
-                # from_idx = pos2idx[from_pos]
-                # to_idx = pos2idx[to_pos]
-                maze.add_edge(from_pos, to_pos)
-    return maze
-
-
 def get_corners():
     return [(x, y, z) for x in [1, 9] for y in [1, 9] for z in [1, 9]]
 
 
-def get_maze():
-    array_xy = str2d_to_array2d(OskarsMaze.xy)
-    array_yz = str2d_to_array2d(OskarsMaze.yz)
-    array_xz = str2d_to_array2d(OskarsMaze.xz)
-    cube = panels_to_cube(array_xy, array_yz, array_xz)
-    maze = cube_to_maze(cube)
-    return maze
-
-
-def panels_to_cube(xy, yz, xz):
-    result = np.zeros((11, 11, 11), dtype=bool)
+def get_cube(xy, yz, xz):
+    xyz = np.zeros((11, 11, 11), dtype=bool)
     for x in range(0, 11):
         for y in range(0, 11):
             for z in range(0, 11):
                 if xy[(x, y)] or yz[(y, z)] or xz[(x, z)]:
-                    result[(x, y, z)] = True
-    # TODO: If a node has only 2 neighbors, and all 3 nodes are collinear,
-    #       then remove that node and join its two neighbors.
-    return result
+                    xyz[(x, y, z)] = True
+    return xyz
 
 
-def str2d_to_array2d(s):
+def get_maze(xyz, do_remove_nonbranching_nodes=True):
+    def tup_sum(a, b):
+        return (a[0]+b[0], a[1]+b[1], a[2]+b[2])
+
+    node_pos = list(zip(*np.where(~xyz)))  # Unobstructed positions
+    nbr_vecs = [(-1,0,0), (1,0,0)] + [(0,-1,0), (0,1,0)] + [(0,0,-1), (0,0,1)]
+    maze = nx.Graph()
+    for from_pos in node_pos:
+        for nbr_vec in nbr_vecs:
+            to_pos = tup_sum(from_pos, nbr_vec)
+            if to_pos in node_pos:  # Both endpoints are graph nodes. Found an edge.
+                maze.add_edge(from_pos, to_pos)
+
+    if do_remove_nonbranching_nodes:
+        # If a node has only 2 neighbors, and all 3 nodes are collinear,
+        # then remove the center node and join its two neighbors.
+        # This process reduces the node count from 220 to 103.
+        for node in list(maze.nodes):
+            nbrs = list(maze.neighbors(node))
+            if len(nbrs) == 2:
+                nbr_a = nbrs[0]
+                nbr_b = nbrs[1]
+                same_x = node[0] == nbr_a[0] == nbr_b[0]
+                same_y = node[1] == nbr_a[1] == nbr_b[1]
+                same_z = node[2] == nbr_a[2] == nbr_b[2]
+                if (same_x and same_y) or (same_x and same_z) or (same_y and same_z):
+                    maze.remove_edge(node, nbr_a)
+                    maze.remove_edge(node, nbr_b)
+                    maze.remove_node(node)
+                    maze.add_edge(nbr_a, nbr_b)
+    return maze
+
+
+def get_panel(s):
     """Convert a 2D string-based cross-sections of the maze to numpy array.
     In the numpy array form, True represents an obstacle; False, an empty space.
     """
@@ -118,19 +123,34 @@ def str2d_to_array2d(s):
     return make_mask(ints).reshape((11, 11))
 
 
-if __name__ == '__main__':
-    maze = get_maze()
-    # assert(maze.number_of_nodes() == 220)  # Before non-branching nodes are removed
-    corners = get_corners()
-    assert_nodes_are_in_maze(maze, corners)
+def make_maze(do_remove_nonbranching_nodes=True):
+    panel_xy = get_panel(OskarsMaze.xy)
+    panel_yz = get_panel(OskarsMaze.yz)
+    panel_xz = get_panel(OskarsMaze.xz)
+    cube = get_cube(panel_xy, panel_yz, panel_xz)
+    maze = get_maze(cube, do_remove_nonbranching_nodes)
+    return maze
 
-    for i in range(len(corners)):
-        for j in range(i + 1, len(corners)):
-            from_pos = corners[i]
-            to_pos = corners[j]
+
+def print_paths(maze, nodes):
+    for i in range(len(nodes)):
+        for j in range(i + 1, len(nodes)):
+            from_pos = nodes[i]
+            to_pos = nodes[j]
             has_path = nx.has_path(maze, from_pos, to_pos)
             if has_path:
                 path = nx.shortest_path(maze, source=from_pos, target=to_pos)
-                print(f'Path from {from_pos} to {to_pos}: shortest length is {len(path)}')
+                print(f'Path from {from_pos} to {to_pos}: Shortest length = {len(path) - 1}')
             else:
                 print(f'Path from {from_pos} to {to_pos}: None')
+
+
+if __name__ == '__main__':
+    maze = make_maze()
+    # print(f'INFO: Maze has {maze.number_of_nodes()} nodes.')
+    # print(f'INFO: Maze has {maze.number_connected_components()} connected components.')
+    nodes = get_corners()
+    assert_nodes_are_in_maze(maze, nodes)
+    print_paths(maze, nodes)
+    with open('oskars_maze.dot', 'w') as f:
+        write_dot(maze, f)  # Convert to png with, e.g., dot -Tpng -o oskars_maze.png oskars_maze.dot
