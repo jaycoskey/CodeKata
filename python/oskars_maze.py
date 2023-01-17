aaca9a8ea5df7ee512b42fabc8b46e0b72161ce8
Jay Coskey <jay.coskey@gmail.com>
2023-01-16 21:11:12 -0800

    Add some type-checking to be used by mypy

--- python/oskars_maze.py
+++ python/oskars_maze.py
@@ -9,11 +9,16 @@
 #
 # Coordinates used range from 0 to 10 for each of the x, y, and z axes.
 
-import networkx as nx
+import networkx as nx  # type: ignore
+from networkx.classes import Graph  # type: ignore
 from networkx.drawing.nx_agraph import write_dot
+from typing import List, TextIO, Tuple, Union
 
 import numpy as np
 from numpy.ma import make_mask
+from numpy.typing import NDArray
+
+Pos = Tuple[int, int, int]
 
 
 # Note: The commercial product does not have labeled axes.
@@ -62,7 +67,7 @@ class OskarsMaze:
         ***********"""
 
     @staticmethod
-    def _get_cube(xy, yz, zx):
+    def _get_cube(xy: NDArray, yz: NDArray, zx: NDArray) -> NDArray:
         xyz = np.zeros((11, 11, 11), dtype=bool)
         for x in range(0, 11):
             for y in range(0, 11):
@@ -72,13 +77,13 @@ class OskarsMaze:
         return xyz
 
     @staticmethod
-    def _get_graph(xyz, do_remove_nonbranching_nodes=True):
+    def _get_graph(xyz: NDArray, do_remove_nonbranching_nodes=True) -> Graph:
         def tup_sum(a, b):
             return (a[0]+b[0], a[1]+b[1], a[2]+b[2])
 
         node_pos = list(zip(*np.where(~xyz)))  # Unobstructed positions
         nbr_vecs = [(-1,0,0), (1,0,0)] + [(0,-1,0), (0,1,0)] + [(0,0,-1), (0,0,1)]
-        graph = nx.Graph()
+        graph: Graph = nx.Graph()  # type: ignore
         for from_pos in node_pos:
             for nbr_vec in nbr_vecs:
                 to_pos = tup_sum(from_pos, nbr_vec)
@@ -105,7 +110,7 @@ class OskarsMaze:
         return graph
 
     @staticmethod
-    def _get_panel(s):
+    def _get_panel(s: str):
         """Convert a 2D string-based cross-sections of the maze to numpy array.
         In the numpy array form, True represents an obstacle; False, an empty space.
         """
@@ -115,43 +120,44 @@ class OskarsMaze:
         return make_mask(ints).reshape((11, 11))
 
     @staticmethod
-    def get_corners():
+    def get_corners() -> List[Pos]:
         return [(x, y, z) for x in [1, 9] for y in [1, 9] for z in [1, 9]]
 
     # ========================================
 
-    def __init__(self, do_remove_nonbranching_nodes=True):
+    def __init__(self, do_remove_nonbranching_nodes=True) -> None:
         panel_xy = OskarsMaze._get_panel(OskarsMaze.xy)
         panel_yz = OskarsMaze._get_panel(OskarsMaze.yz)
         panel_zx = OskarsMaze._get_panel(OskarsMaze.zx)
         cube = OskarsMaze._get_cube(panel_xy, panel_yz, panel_zx)
         self.graph = OskarsMaze._get_graph(cube, do_remove_nonbranching_nodes)
 
-    def number_of_connected_components(self):
-        return nx.number_connected_components(self.graph)
+    def number_of_connected_components(self) -> int:
+        return nx.number_connected_components(self.graph)  # type: ignore
 
-    def number_of_nodes(self):
+    def number_of_nodes(self) -> int:
         return self.graph.number_of_nodes()
 
-    def print_paths(self, nodes):
+    def print_paths(self, nodes: List[Pos]) -> None:
         for i in range(len(nodes)):
             for j in range(i + 1, len(nodes)):
                 from_pos = nodes[i]
                 to_pos = nodes[j]
-                has_path = nx.has_path(self.graph, from_pos, to_pos)
+                has_path = nx.has_path(self.graph, from_pos, to_pos)  # type: ignore
                 if has_path:
-                    path = nx.shortest_path(self.graph, source=from_pos, target=to_pos)
+                    path = nx.shortest_path(self.graph, from_pos, to_pos)  # type: ignore
                     print(f'Path from {from_pos} to {to_pos}: Shortest length = {len(path) - 1}')
                 else:
                     print(f'Path from {from_pos} to {to_pos}: None')
 
-    def write_dot(self, path_or_filehandle):
+    def write_dot(self, path_or_filehandle: Union[str, TextIO]) -> None:
         write_dot(self.graph, f)
 
 
 if __name__ == '__main__':
-    def assert_nodes_are_in_maze(maze, nodes):
-        are_corners_in_maze = [(x, y, z) in maze.graph for (x, y, z) in OskarsMaze.get_corners()]
+    def assert_nodes_are_in_maze(maze: OskarsMaze, nodes):
+        are_corners_in_maze = [(x, y, z) in maze.graph
+                               for (x, y, z) in OskarsMaze.get_corners()]
         assert(all(are_corners_in_maze))
 
     maze = OskarsMaze()
